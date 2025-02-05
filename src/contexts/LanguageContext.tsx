9be1ee1b1d64873e705aff68d672c2dbc334e07a
Rishabh Gusain <rishabhgusain.6@gmail.com>
2025-02-06 01:22:17 +0530

    app interface langauge change

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -10,6 +10,11 @@ const LanguageContext = createContext<LanguageContextType | undefined>(undefined
 export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [language, setLanguage] = useState('en');
 
+  useEffect(() => {
+    // Update HTML lang attribute to trigger browser translation
+    document.documentElement.lang = language;
+  }, [language]);
+
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
       {children}
